13c8af6f63ccafc087fcb3df0ccbb87b79a9de18
Phượng Trịnh <phuong1202112001@gmail.com>
2022-05-19 21:44:14 +0700

    init

---
+++ Assets/Gameplay/Scripts/Static/EnumManager.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnumManager : MonoBehaviour
+{
+    public enum DataType
+    {
+        Integer,
+        Float,
+        String
+    }
+}
